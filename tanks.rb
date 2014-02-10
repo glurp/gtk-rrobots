3889c572f03aaa2d7fec7f63147d989a4f730aa1
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-02-10 19:01:41 +0100

    TankEvil creation

--- tanks.rb
+++ tanks.rb
@@ -27,19 +27,27 @@ class TankDuck < TankGamer
   end
 end
 
-class TankDuck2 < TankGamer
-  def initialize(x,y,coul="#4444FF") 
+class TankEvil < TankGamer
+ def initialize(x,y,coul="#FFAABB") 
     super(x,y,coul) ; 
-    @v= 3
+    @v= 8
     @cradar=0
+    @cible=-1
+    turn(rand(100))
   end
   def tank?() true end
   def anim()
-     #turn_radar(0.5*Math::PI/360)
-     turn_to(Math::PI/16.0)
+     turn_radar(0.5*Math::PI/360)
+     turn(Math::PI/260.0)
      move()
+     if  fire_good?
+       @cdir+=(@cradar-@cdir)/2
+       @cible=@cradar
+     end
+     fire if (@cible-@cdir).abs<0.05
   end
 end
+
 class TankDuck3 < TankGamer
   def initialize(x,y,coul="#44FF44") 
     super(x,y,coul) ; 
