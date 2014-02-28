636a1a9bf863934abf511b95c319e7d957277c93
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-02-28 23:03:59 +0100

    color

--- bullet.rb
+++ bullet.rb
@@ -2,18 +2,17 @@
 
 class Bullet < Tank
   def initialize(origine,x0,y0,dir) 
-    super(x0,y0,"#992222") 
+    super(x0,y0,"#BB2222") 
     @parent,@a,@v=origine,dir,30
   end
-  def draw(w,cr)
-    w.draw_circle(@x,@y,3,@coul,@coul) 
-  end
+  def draw(w,cr) w.draw_circle(@x,@y,3,@coul,@coul)  end
+  def dead()     @parent.dead_bullet end
   def anim(c)
     move()
-    if x==0 || y==0 ||x==200 || y==200
+    if x<=0 || y<=0 ||x>=$W || y>=$H
       $app.dead(self)
       return
     end
-    $app.test_collision(self,x,y)
+    $app.test_collision(self)
   end
 end
