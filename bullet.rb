877461dc6cd07ddfb2529c36d34fa978febf4029
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-03-13 21:13:41 +0100

    bullet size

--- bullet.rb
+++ bullet.rb
@@ -3,7 +3,7 @@
 class Bullet < Tank
   def initialize(origine,x0,y0,dir) 
     super(x0,y0,"#BB2222") 
-    @parent,@a,@v=origine,dir,30
+    @parent,@a,@v=origine,dir,40
   end
   def draw(w,cr) w.draw_circle(@x,@y,3,@coul,@coul)  end
   def dead()     @parent.dead_bullet end
@@ -13,6 +13,6 @@ class Bullet < Tank
       $app.dead(self)
       return
     end
-    $app.test_collision(self)
+    $app.test_collision(self,@parent)
   end
 end
